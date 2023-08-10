28657d9e3ccc3f6ed14d510b6435b7db37b60ad8
Sachin Shrotriy <sachinshrotriy@gmail.com>
2023-08-10 12:39:10 +0530

    slider

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -11,16 +11,16 @@
                               <div class="detail-box">
                                  <h1>
                                     <span>
-                                    Sale 20% Off
+                                    Summer Sale:
                                     </span>
                                     <br>
-                                    On Everything
+                                    Up to 50% Off!
                                  </h1>
                                  <p>
-                                    Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                                 Dive into summer savings with our up to 50% off sale. Explore a wide range of stylish and comfortable outfits for a limited time. Don't miss out on this chance to refresh your wardrobe with the latest trends at incredible prices.
                                  </p>
                                  <div class="btn-box">
-                                    <a href="" class="btn1">
+                                    <a href="{{url('products')}}" class="btn1">
                                     Shop Now
                                     </a>
                                  </div>
@@ -36,16 +36,16 @@
                               <div class="detail-box">
                                  <h1>
                                     <span>
-                                    Sale 20% Off
+                                    20% Off
                                     </span>
                                     <br>
-                                    On Everything
+                                    Family Offer
                                  </h1>
                                  <p>
-                                    Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                                 Create unforgettable family moments with our 20% off matching outfits promotion. Coordinate your look with loved ones and capture memories that will last a lifetime, all while enjoying special discounts..
                                  </p>
                                  <div class="btn-box">
-                                    <a href="" class="btn1">
+                                    <a href="{{url('products')}}" class="btn1">
                                     Shop Now
                                     </a>
                                  </div>
@@ -61,16 +61,16 @@
                               <div class="detail-box">
                                  <h1>
                                     <span>
-                                    Sale 20% Off
+                                    10% Off
                                     </span>
                                     <br>
-                                    On Everything
+                                    Entire Collection
                                  </h1>
                                  <p>
-                                    Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                                 Embrace the upcoming season with a fresh look at 10% off our entire collection. From trendy apparel to must-have accessories, revamp your style and step into the new season with confidence and savings.
                                  </p>
                                  <div class="btn-box">
-                                    <a href="" class="btn1">
+                                    <a href="{{url('products')}}" class="btn1">
                                     Shop Now
                                     </a>
                                  </div>
